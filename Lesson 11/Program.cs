524f31cfc08ab964737f2ee4e7763fbf772f72da
Artem Viatkin <newyorkbox47@gmail.com>
2022-12-29 14:35:28 +0200

    Добавьте файлы проекта.

---
+++ Lesson 11/Program.cs
@@ -0,0 +1,14 @@
+﻿using Lesson_11;
+
+Console.WriteLine("****Fun with Static Data*****\n");
+SavingsAccount s1 = new SavingsAccount(50);
+SavingsAccount s2 = new SavingsAccount(100);
+
+//выести текущую процентную ставку
+Console.WriteLine("Interest Rate is:{0}", SavingsAccount.GetInterestRate());
+
+//создаем новій обьект
+SavingsAccount s3 = new SavingsAccount(10000.75);
+Console.WriteLine("Interest Rate is:{0}", SavingsAccount.GetInterestRate());
+
+Console.ReadLine();
---
+++ Lesson 11/SavingsAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11
+{
+    //простой класс депозитного счета 
+    class SavingsAccount
+    {
+        //данные уровня экземпляра
+        public double currBalance;
+
+        //cтатический эл-т данных
+        public static double currInterestRate = 0.04;
+        
+        static SavingsAccount() //статический сконструктор
+        {
+            Console.WriteLine("In static ctor"); //в статическом конструкторе
+            currInterestRate = 0.04;
+            //перегружать статический конструктор нельзя
+            //статический конструктор не может принимать параметры и иметь мод доступа
+            //статический конструктор выполняется только один раз 
+        }
+        public SavingsAccount(double balance)
+        {
+            currBalance = balance;
+        }
+        //статические члены для установки/получения процентной ставки
+        //статические данные безупречны в ситуации, когда есть значение,
+        //которое должно быть общиим для всех обьектов в заданой категории
+        public static void SetInterestRate(double newRate)
+         => currInterestRate = newRate;
+
+        public static double GetInterestRate()
+         => currInterestRate;   
+    }
+}
